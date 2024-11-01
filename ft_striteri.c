96cb5c5171c8da239ca6f127d904111457218f83
Ilyas <ilyasilyas17062003@gmail.com>
2024-11-01 13:47:07 +0100

    /:

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,23 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ft_strmapi.c                                      :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: irfei <irfei@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/26 12:24:46 by irfei             #+#    #+#             */
-/*   Updated: 2024/10/27 17:38:54 by irfei            ###   ########.fr       */
+/*   Created: 2024/10/31 19:41:59 by irfei             #+#    #+#             */
+/*   Updated: 2024/10/31 19:50:08 by irfei            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	char *rezult;
+	char *result;
 	size_t len;
-	size_t i;
+	unsigned int i;
 	if (!s)
-		return (ft_strdup "");
-		
+		return ;
+	len = ft_strlen(s);
+	result = (char *)malloc(sizeof(char) * (len + i));
+	if (!result)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return ;
 }
